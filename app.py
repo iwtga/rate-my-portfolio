018de11133fedaaa328ba2317352b55bcac8c038
Alan Almeida <alan241015@gmail.com>
2021-01-23 12:46:03 +0530

    Add Sigup and login with app.py integration

--- app.py
+++ app.py
@@ -3,8 +3,25 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.errorhandler(404) 
+def not_found(e): 
+  return render_template("404.html")
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
