3c8d20ee1b1bed7c00f2c540a0127d39a805433f
Warren <warren.fernandes27@gmail.com>
2021-01-23 15:34:14 +0530

    Firebase authentication completed

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, url_for, redirect, request
+import pyrebase
 
 app = Flask(__name__)
+
+config ={
+    "apiKey": "AIzaSyDHpOYyNgtYcrnCORHLHLyPDK4bdgY4_Vs",
+    "authDomain": "ratemyportfolio-39e05.firebaseapp.com",
+    "databaseURL": "https://ratemyportfolio-39e05-default-rtdb.firebaseio.com/",
+    "projectId": "ratemyportfolio-39e05",
+    "storageBucket": "ratemyportfolio-39e05.appspot.com",
+    "messagingSenderId": "115042671407",
+    "appId": "1:115042671407:web:403f102924080cf89b1fda",
+    "measurementId": "G-RMQFGMEQYV"
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
 database = {'warren': 'warren271001'}
 
@@ -24,20 +38,28 @@ def loginpage():
 @app.route('/form_login', methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        name = request.form['username']
+        email = request.form['email']
         password = request.form['password']
-        if name not in database:
-            return render_template('error.html', errorcode="User Not Found", errormessage="Username does not exist")
+        if auth.sign_in_with_email_and_password(email,password):
+            return render_template('home.html')
         else:
-            if database[name] != password:
-                return render_template('error.html', errorcode="Incorrect Password", erromessage="Password is Incorrect")
-            else:
-                return render_template('home.html')
+            return  render_template('error.html')
+        
+@app.route('/signup')
+def signuppage():
+    return render_template('signup.html')
+
+@app.route('/form_signup', methods=["POST","GET"])
+def signup():
+    unsuccessful = 'Please check your credentials'
+    successful = 'Login successful'
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        auth.create_user_with_email_and_password(email,password)
+        return render_template('home.html')
         
     
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
